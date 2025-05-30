b4391496b2fc140a86cd19cfbd6c6044c0aee029
FABIPORTELA <168192189+FABIPORTELA@users.noreply.github.com>
2025-05-30 09:31:12 -0300

    oii

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// 1. Variável para armazenar a contagem
+let cliques = 0;
+
+// 2. Seleciona os elementos da página
+const botao = document.getElementById('botaoClique');
+const areaContador = document.getElementById('contador');
+
+// 3. Adiciona o evento de clique ao botão
+botao.addEventListener('click', function () {
+  // 4. Incrementa o valor e atualiza o contador
+  cliques++;
+  areaContador.textContent = cliques;
+});
 
